d099c29063a7a1f88111e125a8911408550b2236
nicolas.nos <nicolas.nos@hotmail.com>
2024-04-01 12:41:56 -0500

    the navigation working between randoom coctel and perro

--- App.js
+++ App.js
@@ -1,28 +1,11 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { NativeRouter } from "react-router-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import RandomCoctel from "./Components/Random";
+import Main from "./Components/Main";
 
 export default function App() {
   return (
     <NativeRouter>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <RandomCoctel />
-      </View>
+      <Main />
     </NativeRouter>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f5f5f5",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -1,12 +1,34 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Constants from "expo-constants";
+import { Link } from "react-router-native";
 
+const AppBar = ({ active, to, children }) => {
+  return (
+    <Link to={to}>
+      <Text style={styles.text}>{children}</Text>
+    </Link>
+  );
+};
 export default function NavBar() {
   return (
-    <View>
-      <Text>NavBar</Text>
+    <View style={styles.container}>
+      <AppBar to={"/"}>Random coctel</AppBar>
+      <AppBar to={"/perro"}>GUAGU</AppBar>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    width: 150,
+    paddingTop: Constants.statusBarHeight + 75,
+    paddingBottom: 10,
+    paddingLeft: 10,
+  },
+  text: {
+    color: "#f5f5f5",
+    fontWeight: "bold",
+  },
+});
